6269a2140ff78f9b8eddd2d9a8340d8dd693a8a2
Tino Kallinich <tkallinich-friedrich@inovex.de>
2021-01-12 12:02:13 +0100

    added python kata reverse string

--- phytify/Main.py
+++ phytify/Main.py
@@ -5,11 +5,28 @@ class Main:
         while n != 0:
             result += pow(n % 10, 2)
             n /= 10
-
+        print("Happy Numbers: " + str(result))
         return result
+
+    def reverseString(self, inputString):
+        result = ""
+        list = []
+
+        for char in inputString:
+            list.append(char)
+
+        length = len(list)
+
+        for i in range(length):
+            length -= 1
+            result += list[length]
+
+        print("Result  String: " + str(inputString))
+        print("Reverse String: " + str(result))
 
 
 # always two space between
 if __name__ == '__main__':
     main = Main()
-    print("Happy Numbers: " + str(main.run(19)))
+    # main.run(19)
+    main.reverseString("Hello World")
