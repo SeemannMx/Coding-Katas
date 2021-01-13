8dac4a4bad02068cfcab9a5128809c38bc45954d
Tino Kallinich <tkallinich-friedrich@inovex.de>
2021-01-13 13:02:11 +0100

    added reverse string by swift

--- swiftify/swiftify.playground/Contents.swift
+++ swiftify/swiftify.playground/Contents.swift
@@ -8,16 +8,25 @@ var digit = 0
 
 while n != 0 {
     digit = n % 10
-    print("Digit ", digit)
-    
     pow = digit * digit
-    print("Power ", pow)
-    
     result = result + pow
 
     n = n / 10
-    print("N ", n)
 }
 
-print("----------------")
-print("Result ", result)
+print("Happy Number: ", result)
+
+var inputtString = "Hello World"
+var resultString = ""
+var lenght = inputtString.count
+
+print("input  String: ", inputtString)
+
+while lenght != 0 {
+    var char = inputtString[inputtString.index(inputtString.endIndex, offsetBy: -1 * lenght)]
+    print("char: ", char)
+    
+    lenght = lenght - 1
+}
+
+print("result String: ", resultString)
